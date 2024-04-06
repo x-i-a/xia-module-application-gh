a47487a237f1f7125a1951c86b765fed311bb743
X-I-A <develop@x-i-a.com>
2024-04-06 17:42:40 +0200

    feat: explict module declaration

--- xia_module_application_gh/__init__.py
+++ xia_module_application_gh/__init__.py
@@ -2,11 +2,11 @@ from xia_module_application_gh.application import Application
 
 
 modules = {
-    "gh-module-application": Application
+    "gh-module-application": "Application"
 }
 
 __all__ = [
     "Application"
 ]
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
