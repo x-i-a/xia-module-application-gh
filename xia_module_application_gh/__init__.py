001f357be39d38ca31993a560dfdf2fed29bd3a4
X-I-A <develop@x-i-a.com>
2024-08-21 21:00:13 +0200

    feat: cosmos level rework

--- xia_module_application_gh/__init__.py
+++ xia_module_application_gh/__init__.py
@@ -1,12 +1,15 @@
 from xia_module_application_gh.application import Application
+from xia_module_application_gh.foundation import Foundation
 
 
 modules = {
-    "gh-module-application": "Application"
+    "gh-module-application": "Application",
+    "gh-module-foundation": "Foundation"
 }
 
 __all__ = [
-    "Application"
+    "Application",
+    "Foundation"
 ]
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
---
+++ xia_module_application_gh/foundation.py
@@ -0,0 +1,5 @@
+from xia_module import Module
+
+
+class Foundation(Module):
+    module_name = "gh-module-foundation"
