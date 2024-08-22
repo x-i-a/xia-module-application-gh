1bad8e7fcf27b6b193aeb100c625d926b5c7b0b0
X-I-A <develop@x-i-a.com>
2024-08-22 10:57:34 +0200

    feat: cosmos level rework

--- xia_module_application_gh/templates/gh-module-application/base/activate.tf
+++ xia_module_application_gh/templates/gh-module-application/base/activate.tf
@@ -1,6 +1,7 @@
 module "activate_gh_module_application" {
   source = "../../modules/activate-gh-module-application"
 
+  config_file = "../../../config/core/github.yaml"
   landscape = local.landscape
   foundations = local.foundations
 
