3d7897e62d1eaf5dd6052abe5e9087bdeda2a33e
X-I-A <develop@x-i-a.com>
2024-08-22 08:05:25 +0200

    feat: cosmos level rework

--- xia_module_application_gh/templates/gh-module-application/activate/main.tf
+++ xia_module_application_gh/templates/gh-module-application/activate/main.tf
@@ -9,3 +9,10 @@ terraform {
 locals {
   module_name = coalesce(var.module_name, substr(basename(path.module), 9, length(basename(path.module)) - 9))
 }
+
+resource "github_team" "foundation_admin" {
+  for_each = var.foundations
+  name        = "${each.value["name"]}-adm"
+  description = "Foundation ${each.value["name"]} Administrator Team"
+  privacy     = "closed"
+}
