03dbe4dd2853150b334a652651f16395ea255b72
X-I-A <develop@x-i-a.com>
2024-08-25 21:23:21 +0200

    feat: module dependent providers

--- xia_module_application_gh/templates/gh-module-application/activate/main.tf
+++ xia_module_application_gh/templates/gh-module-application/activate/main.tf
@@ -10,14 +10,6 @@ data "github_user" "current" {
   username = ""
 }
 
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-    }
-  }
-}
-
 locals {
   module_name = coalesce(var.module_name, substr(basename(path.module), 9, length(basename(path.module)) - 9))
 
