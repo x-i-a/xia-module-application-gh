b5cfc74ed18d82dca632cf1cf75468097b596969
X-I-A <develop@x-i-a.com>
2024-09-08 20:15:34 +0200

    feat: init config for tfstate.yaml

--- xia_module_application_gh/foundation.py
+++ xia_module_application_gh/foundation.py
@@ -3,3 +3,20 @@ from xia_module import Module
 
 class Foundation(Module):
     module_name = "gh-module-foundation"
+
+    def init_config(self, repo_dict: dict = None, var_dict: dict = None, **kwargs):
+        """Initialization of configuration file
+
+        Args:
+            repo_dict (dict): Repository Information
+            var_dict (dict): Repository Variable Dictionary
+            **kwargs: Parameter to be used for configuration file changes
+        """
+        repo_dict, var_dict = (repo_dict or {}), (var_dict or {})
+        config_file, config_dir = self.get_config_file_path()
+        if "repo" in repo_dict:
+            github_owner_name = var_dict["repo"]
+            github_replace_dict = {
+                "github_owner:": f"github_owner: {github_owner_name}\n",
+            }
+            self._config_replace(config_file, github_replace_dict)
